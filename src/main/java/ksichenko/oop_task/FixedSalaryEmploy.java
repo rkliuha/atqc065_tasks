ecf5e1d3786d885a5436e07e29ed80add8b1d210
kostyantins <kostyantins@mail.ru>
2016-08-25 18:28:10 +0300

    task2 with corrections

--- src/main/java/ksichenko/oop_task/FixedSalaryEmploy.java
+++ src/main/java/ksichenko/oop_task/FixedSalaryEmploy.java
@@ -2,13 +2,15 @@ package ksichenko.oop_task;
 
 public class FixedSalaryEmploy extends Employ {
 
-    public FixedSalaryEmploy(String name, String sex, String salary, int id) {
-        super(name, sex, salary, id);
+    public FixedSalaryEmploy(String name, String sex, int salary, int id) {
+        this.name = name;
+        this.sex = sex;
+        this.salary = salary;
+        this.id = id;
     }
 
     void calculateSalary() {
-
-        int salary = 10000;
+        salary = 10000;
         System.out.println("Employ salary amount: " + salary);
     }
 }

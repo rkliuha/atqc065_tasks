c761ba302bcc1dd82beea8c0985dd6908ff543b4
Kornienko Oleksandr <drevnij67@gmail.com>
2016-08-30 00:56:23 +0300

    task 4-5a

--- src/main/java/okornienko/oop_task/FixedSalaryEmployee.java
+++ src/main/java/okornienko/oop_task/FixedSalaryEmployee.java
@@ -1,18 +1,13 @@
 package okornienko.oop_task;
 
-/**
- * Created by Aleksandr on 25-08-16.
- */
-public class FixedSalaryEmployee extends Employees  {
-    FixedSalaryEmployee(int employeeId, String employeeName, String employeeSex, double employeeSalary) {
+public class FixedSalaryEmployee extends Employees {
+    FixedSalaryEmployee(final int employeeId, final String employeeName, final String employeeSex, final double employeeSalary) {
         this.employeeId = employeeId;
         this.employeeName = employeeName;
         this.employeeSex = employeeSex;
         this.employeeSalary = employeeSalary;
     }
 
-  public final double calculateSalary() {
-        return employeeSalary;
+    public final void calculateSalary() {
     }
-
 }
